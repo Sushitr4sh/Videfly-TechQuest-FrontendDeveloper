59b5e0b8c862652bc30069d07487811d5cd1409a
Mario Imanuel Daruranto <mariodaruranto68@gmail.com>
2024-11-11 14:54:57 +0700

    Fix sidebar gap

--- src/components/MiscIconButton.jsx
+++ src/components/MiscIconButton.jsx
@@ -4,7 +4,7 @@ const MiscIconButton = ({ isNotifOpen, onSelect }) => {
   return (
     <div className="relative">
       <button
-        onClick={() => onSelect(!isNotifOpen)}
+        onClick={() => onSelect(true)}
         className="flex justify-center items-center rounded-lg hover:bg-[#d3c2f8] transition duration-200"
       >
         <img src="/icons/notification-01.svg" alt="notification icon" />
