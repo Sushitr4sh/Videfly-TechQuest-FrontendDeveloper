dfbe1c751a672c1d05e7512904da4bc8af43b8a9
Mario Imanuel Daruranto <mariodaruranto68@gmail.com>
2024-11-14 06:16:22 +0700

    Add project page section page

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -19,54 +19,152 @@ const useHoverSupport = () => {
   return isHoverSupported;
 };
 
-const Carousel = ({ items, project = false }) => {
+const Carousel = ({ items, type, filter = "semua" }) => {
   const [activeIndex, setActiveIndex] = useState(null);
   const [showOption, setShowOption] = useState(null);
   const isHoverSupported = useHoverSupport();
 
   return (
     <div className="relative">
-      <div className="overflow-x-auto flex scrollbar-hide gap-x-6 snap-x snap-mandatory pb-3 lg:pb-2 content-scrollbar">
-        {items.map((item, index) => (
-          <div
-            onMouseEnter={() => isHoverSupported && setShowOption(index)}
-            onMouseLeave={() => isHoverSupported && setShowOption(null)}
-            onTouchStart={() => setShowOption(index)}
-            key={index}
-            className={`${
-              project ? "w-[15.375rem] h-[15rem]" : "w-[15.875rem]"
-            }  pb-3 border border-[#ececec] rounded-2xl shrink-0 flex items-center justify-center overflow-hidden relative snap-center`}
-          >
-            <div className="w-full">
-              <img
-                src={item.src}
-                className="object-cover object-top w-full rounded-2xl"
-                alt="Carousel Image"
-              />
-              <div className="flex items-center text-white justify-between w-full absolute top-0 p-2">
-                <div className="flex items-center gap-x-1">
-                  {item.duration !== "Draf" && (
-                    <TimeButton visible={true}>Render</TimeButton>
-                  )}
-                  <TimeButton visible={true}>{item.duration}</TimeButton>
-                </div>
-                <TimeButton
-                  type="icon"
-                  visible={showOption === index || !isHoverSupported}
-                >
-                  <Icon
-                    icon="pepicons-pencil:dots-x"
-                    width={"24px"}
-                    height={"24px"}
+      <div
+        className={`${
+          filter === "semua"
+            ? "overflow-x-auto flex scrollbar-hide gap-x-6 snap-x snap-mandatory pb-3 lg:pb-2 content-scrollbar"
+            : "w-full flex flex-col justify-center items-center gap-y-6"
+        }`}
+      >
+        {type !== "file"
+          ? items.map((item, index) => (
+              <div
+                onMouseEnter={() => isHoverSupported && setShowOption(index)}
+                onMouseLeave={() => isHoverSupported && setShowOption(null)}
+                onTouchStart={() => setShowOption(index)}
+                key={index}
+                className={`${
+                  type === "content"
+                    ? "w-[15.375rem] h-[15rem]"
+                    : "w-[15.875rem]"
+                } pb-3 border border-[#ececec] rounded-2xl shrink-0 flex items-center justify-center overflow-hidden relative snap-center`}
+              >
+                <div className="w-full">
+                  <img
+                    src={item.src}
+                    className="object-cover object-top w-full rounded-2xl"
+                    alt="Carousel Image"
                   />
-                </TimeButton>
+                  <div className="flex items-center text-white justify-between w-full absolute top-0 p-2">
+                    <div className="flex items-center gap-x-1">
+                      {item.duration !== "Draf" && (
+                        <TimeButton visible={true}>Render</TimeButton>
+                      )}
+                      <TimeButton visible={true}>{item.duration}</TimeButton>
+                    </div>
+                    <TimeButton
+                      type="icon"
+                      visible={showOption === index || !isHoverSupported}
+                    >
+                      <Icon
+                        icon="pepicons-pencil:dots-x"
+                        width={"24px"}
+                        height={"24px"}
+                      />
+                    </TimeButton>
+                  </div>
+                  <div className="flex justify-between px-3 mt-3">
+                    <div className="flex flex-col gap-y-1">
+                      <p className="text-sm lg:text-xs font-semibold text-[#111336]">
+                        {item.title}
+                      </p>
+                      <div className="flex justify-between gap-x-2 items-center">
+                        <p
+                          className={`text-[0.688rem] text-[#545454] ${
+                            !item.status && "py-[0.125rem]"
+                          }`}
+                        >
+                          {item.date}
+                        </p>
+                        {item.status && (
+                          <>
+                            <img src="/icons/separator2.svg" alt="separator" />
+                            <button className="bg-[#E3FFE5] rounded-md px-2 py-[0.125rem] font-medium text-[#60D669] text-[0.688rem]">
+                              {item.status}
+                            </button>
+                          </>
+                        )}
+                      </div>
+                    </div>
+                    <button
+                      onMouseDown={() => setActiveIndex(index)}
+                      onMouseUp={() => setActiveIndex(null)}
+                      onTouchStart={() => setActiveIndex(index)}
+                      onTouchEnd={() => setActiveIndex(null)}
+                      className={`flex justify-center items-center ${
+                        activeIndex === index
+                          ? "bg-[#8D62EC]"
+                          : "bg-black/5 hoverable:hover:bg-black/10"
+                      } w-9 h-9 rounded-lg transition duration-200`}
+                    >
+                      <div
+                        className="transition-colors duration-200"
+                        style={{
+                          color: activeIndex === index ? "#FFFFFF" : "#111936",
+                        }}
+                      >
+                        <ScissorIcon size={18} />
+                      </div>
+                    </button>
+                  </div>
+                </div>
               </div>
-              <div className="flex justify-between px-3 mt-3">
-                <div className="flex flex-col gap-y-1">
+            ))
+          : items.map((item, index) => (
+              <div
+                key={index}
+                className="w-[12.75rem] p-4 rounded-2xl border border-[#E6E6E6] flex flex-col gap-y-3 snap-center shrink-0"
+              >
+                <div className="flex gap-x-3">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="48"
+                    height="48"
+                    viewBox="0 0 48 48"
+                    fill="none"
+                  >
+                    <rect
+                      x="0.5"
+                      y="0.5"
+                      width="47"
+                      height="47"
+                      rx="9.5"
+                      stroke="#8A8A8A"
+                      strokeDasharray="4 4"
+                    />
+                  </svg>
+                  <div className="flex">
+                    {item.src.map((i, index) => (
+                      <img
+                        key={index}
+                        src={i}
+                        alt="berkas image"
+                        className="w-12 h-12 rounded-[0.625rem] object-cover -ml-4 first:ml-0 border border-white"
+                        style={{ zIndex: item.src.length - index }}
+                      />
+                    ))}
+                  </div>
+                </div>
+                <div className="flex flex-col">
                   <p className="text-sm lg:text-xs font-semibold text-[#111336]">
                     {item.title}
                   </p>
-                  <div className="flex justify-between gap-x-2 items-center">
+                  <div className="flex gap-x-[0.375rem] items-center">
+                    <p
+                      className={`text-[0.688rem] text-[#545454] ${
+                        !item.status && "py-[0.125rem]"
+                      }`}
+                    >
+                      {item.total} Item
+                    </p>
+                    <img src="/icons/separator2.svg" alt="separator" />
                     <p
                       className={`text-[0.688rem] text-[#545454] ${
                         !item.status && "py-[0.125rem]"
@@ -74,40 +172,10 @@ const Carousel = ({ items, project = false }) => {
                     >
                       {item.date}
                     </p>
-                    {item.status && (
-                      <>
-                        <img src="/icons/separator2.svg" alt="separator" />
-                        <button className="bg-[#E3FFE5] rounded-md px-2 py-[0.125rem] font-medium text-[#60D669] text-[0.688rem]">
-                          {item.status}
-                        </button>
-                      </>
-                    )}
                   </div>
                 </div>
-                <button
-                  onMouseDown={() => setActiveIndex(index)}
-                  onMouseUp={() => setActiveIndex(null)}
-                  onTouchStart={() => setActiveIndex(index)}
-                  onTouchEnd={() => setActiveIndex(null)}
-                  className={`flex justify-center items-center ${
-                    activeIndex === index
-                      ? "bg-[#8D62EC]"
-                      : "bg-black/5 hoverable:hover:bg-black/10"
-                  } w-9 h-9 rounded-lg transition duration-200`}
-                >
-                  <div
-                    className="transition-colors duration-200"
-                    style={{
-                      color: activeIndex === index ? "#FFFFFF" : "#111936",
-                    }}
-                  >
-                    <ScissorIcon size={18} />
-                  </div>
-                </button>
               </div>
-            </div>
-          </div>
-        ))}
+            ))}
       </div>
     </div>
   );
--- src/routes/Project.jsx
+++ src/routes/Project.jsx
@@ -34,6 +34,20 @@ const carouselItems = [
     src: "/images/carousel.png",
   },
 ];
+const berkasItems = [
+  {
+    title: "Background",
+    total: 1,
+    date: "10 menit lalu",
+    src: ["/images/files1.jpg"],
+  },
+  {
+    title: "Aset Mockup",
+    total: 4,
+    date: "15 menit lalu",
+    src: ["/images/files2.png", "/images/files2_1.png", "/images/files2_2.png"],
+  },
+];
 
 const filters = ["Semua", "Konten", "Berkas", "Gambar", "Video", "Sampah"];
 
@@ -90,14 +104,96 @@ const Project = () => {
             onSelect={setFilter}
           />
         </div>
-        <div className="mt-6 flex flex-col gap-y-4">
-          <MinMaxButton>Konten</MinMaxButton>
-          <Carousel items={carouselItems} project={true} />
-        </div>
-        <div className="mt-6 flex flex-col gap-y-4">
-          <MinMaxButton>Berkas</MinMaxButton>
-          <Carousel items={carouselItems} project={true} />
-        </div>
+
+        {/* Semua */}
+        {filter === "Semua" && (
+          <>
+            <div className="mt-6 flex flex-col gap-y-4">
+              <MinMaxButton>Konten</MinMaxButton>
+              <Carousel items={carouselItems} type="content" />
+            </div>
+            <div className="mt-6 flex flex-col gap-y-4">
+              <MinMaxButton>Berkas</MinMaxButton>
+              <Carousel items={berkasItems} type="file" />
+            </div>
+            <div className="mt-6 flex flex-col gap-y-4">
+              <MinMaxButton>Gambar</MinMaxButton>
+              <Carousel items={carouselItems} type="content" />
+            </div>
+            <div className="mt-6 flex flex-col gap-y-4">
+              <MinMaxButton>Video</MinMaxButton>
+              <Carousel items={carouselItems} type="content" />
+            </div>
+          </>
+        )}
+
+        {/* Konten */}
+        {filter === "Konten" && (
+          <div className="mt-6">
+            <Carousel items={carouselItems} filter={filter} type="content" />
+          </div>
+        )}
+
+        {/* Berkas */}
+        {filter === "Berkas" && (
+          <div className="mt-6">
+            <Carousel items={berkasItems} type="file" filter={filter} />
+          </div>
+        )}
+
+        {/* Gambar */}
+        {filter === "Gambar" && (
+          <div className="mt-6">
+            <Carousel items={carouselItems} filter={filter} type="content" />
+          </div>
+        )}
+
+        {/* Video */}
+        {filter === "Video" && (
+          <div className="mt-6">
+            <Carousel items={carouselItems} filter={filter} type="content" />
+          </div>
+        )}
+
+        {/* Sampah */}
+        {filter === "Sampah" && (
+          <div className="mt-6">
+            <div className="flex items-center gap-x-2 mb-6">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="14"
+                height="14"
+                viewBox="0 0 14 14"
+                fill="none"
+              >
+                <path
+                  d="M12.8332 6.99935C12.8332 3.77769 10.2215 1.16602 6.99984 1.16602C3.77817 1.16602 1.1665 3.77769 1.1665 6.99935C1.1665 10.221 3.77817 12.8327 6.99984 12.8327C10.2215 12.8327 12.8332 10.221 12.8332 6.99935Z"
+                  stroke="#8A8A8A"
+                  stroke-width="1.5"
+                />
+                <path
+                  d="M7.14144 9.91602V6.99935C7.14144 6.72437 7.14144 6.58687 7.05598 6.50142C6.97058 6.41602 6.83309 6.41602 6.55811 6.41602"
+                  stroke="#8A8A8A"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M6.99561 4.66602H7.00086"
+                  stroke="black"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+              <p className="text-xs text-[#8A8A8A] tracking-[1.2px]">
+                Konten atau item akan tersimpan di sini selama 30 hari sebelum
+                dihapus secara otomatis.
+              </p>
+            </div>
+            <Carousel items={carouselItems} filter={filter} />
+          </div>
+        )}
       </main>
     </>
   );
